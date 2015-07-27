11de690d98bd47c3bcb14c0163a89ac8ec13ee8c
Stuart Colville <scolville@mozilla.com>
2015-07-27 15:29:06 +0100

    Configure redux-thunk

--- public/js/redux-config.js
+++ public/js/redux-config.js
@@ -1,8 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import * as reducers from 'reducers';
 
 export function createReduxStore() {
-  return createStore(combineReducers(reducers));
+  const reducer = combineReducers(reducers);
+  const finalCreateStore = applyMiddleware(thunk)(createStore);
+  return finalCreateStore(reducer);
 }
 
 export default createReduxStore();
