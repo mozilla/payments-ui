00cb34569dc5813e4191de0613b4b82bee8d9415
Kumar McMillan <kumar.mcmillan@gmail.com>
2015-08-05 12:22:19 -0500

    Use middleware for redux logging

--- public/js/reducers/app.js
+++ public/js/reducers/app.js
@@ -4,7 +4,6 @@ import * as actionTypes from 'constants/action-types';
 export default function app(state, action) {
 
   if (action.type === actionTypes.APP_ERROR) {
-    console.log('app store: got action', action);
     return {
       error: {
         debugMessage: action.error.debugMessage,
--- public/js/reducers/user.js
+++ public/js/reducers/user.js
@@ -13,7 +13,6 @@ export const initialUserState = {
 export default function user(state, action) {
 
   if (action.type === actionTypes.USER_SIGNED_OUT) {
-    console.log('user store: got action', action);
     return Object.assign({}, initialUserState, {
       signedIn: false,
     });
@@ -28,42 +27,36 @@ export default function user(state, action) {
   }
 
   if (action.type === actionTypes.GOT_BRAINTREE_TOKEN) {
-    console.log('user: got action', action);
     return Object.assign({}, initialUserState, state, {
       braintreeToken: action.braintreeToken,
     });
   }
 
   if (action.type === actionTypes.LOADING_USER_SUBSCRIPTIONS) {
-    console.log('user: initializing from', action);
     return Object.assign({}, initialUserState, state, {
       subscriptions: initialUserState.subscriptions,
     });
   }
 
   if (action.type === actionTypes.GOT_USER_SUBSCRIPTIONS) {
-    console.log('user: storing subscriptions from', action);
     return Object.assign({}, initialUserState, state, {
       subscriptions: action.subscriptions,
     });
   }
 
   if (action.type === actionTypes.GOT_PAY_METHODS) {
-    console.log('user store: got action', action);
     return Object.assign({}, initialUserState, state, {
       payMethods: action.payMethods,
     });
   }
 
   if (action.type === actionTypes.ADD_PAY_METHOD) {
-    console.log('user store: got action', action);
     return Object.assign({}, initialUserState, state, {
       payMethods: action.payMethods,
     });
   }
 
   if (action.type === actionTypes.DEL_PAY_METHOD) {
-    console.log('user store: got action', action);
     return Object.assign({}, initialUserState, state, {
       payMethods: action.payMethods,
     });
