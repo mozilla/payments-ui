0ac7e69613928c4bf064e6eb11289c6b33436cba
Payments <payments-devs-automation@mozilla.com>
2015-08-11 20:17:22 +0000

    publish docker build branch (auto)
    branch:       master
    SHA:          1172138b106e809c90fef0c30db9e2d1739be7e7
    range SHA:    9d3d19559a13...1172138b106e
    build id:     75153893
    build number: 471

--- public/dist/management.bundle.js
+++ public/dist/management.bundle.js
@@ -31975,35 +31975,56 @@ webpackJsonp([0,2],[
 	    enabled: false,
 	    id: 'UA-35433268-60'
 	  },
-	  // FxA relier client IDs for user sign-in (not token sign in).
-	  // These are docker development client IDs mapped by host.
-	  fxaClientIdMap: {
-	    'pay.dev:8000': '8d7c6c8549cc6deb',
-	    'pay.webpack:8080': 'a63657a4c78dd650'
-	  },
-	  fxaRedirectUri: 'http://pay.dev:8000',
+	
+	  // FxA client ID (configured per host).
+	  // This is used for user sign-in, not token sign in.
+	  // The payments-service API must be configured with each
+	  // corresponding secret.
+	  fxaClientId: undefined,
+	  fxaRedirectUri: undefined,
 	  fxaContentHost: 'https://stable.dev.lcip.org',
 	  fxaOauthHost: 'https://oauth-stable.dev.lcip.org/v1',
+	  apiPrefix: '/api',
+	  allowCORSRequests: false,
 	
-	  apiPrefix: 'http://pay.dev:8000/api',
-	  allowCORSRequests: false
+	  // This is a map of custom settings that will be defined
+	  // based on host.
+	  hostSettings: {
+	    'pay.dev.mozaws.net': {
+	      fxaClientId: '90f432a069a26c77',
+	      fxaRedirectUri: 'http://pay.dev.mozaws.net/'
+	    },
+	    // These are only used locally for docker.
+	    'pay.dev:8000': {
+	      fxaClientId: '8d7c6c8549cc6deb',
+	      fxaRedirectUri: 'http://pay.dev:8000'
+	    },
+	    'pay.webpack:8080': {
+	      fxaClientId: 'a63657a4c78dd650',
+	      fxaRedirectUri: 'http://pay.webpack:8080',
+	      // Webpack needs to talk via CORS to the dev API.
+	      // See payments-service for how this is allowed.
+	      apiPrefix: 'http://pay.dev:8000/api',
+	      allowCORSRequests: true
+	    }
+	  }
 	};
 	
 	if (typeof window !== 'undefined') {
 	  var host = window.location.host;
-	  module.exports.fxaClientId = module.exports.fxaClientIdMap[host];
+	  var hostSettings = module.exports.hostSettings[host];
 	
-	  if (!module.exports.fxaClientId) {
-	    console.warn('no FxA client ID has been configured for host ' + host);
+	  if (!hostSettings) {
+	    // For example, the unit tests are served by localhost which is unmapped.
+	    console.warn('no custom settings for host ' + host);
 	  } else {
-	    // At this point we have a client ID which means the app is running
-	    // on a known, whitelisted host. It is ok to allow CORS requests
-	    // but note that the API server still has to support our host.
-	    console.info('app is on a known host; we will allow CORS requests');
-	    module.exports.allowCORSRequests = true;
+	    console.log('applying custom settings for host ' + host);
+	    for (var attr in hostSettings) {
+	      module.exports[attr] = hostSettings[attr];
+	    }
 	  }
 	} else {
-	  console.warn('probably not in a web browser; skipping some configuration');
+	  console.warn('probably not in a web browser; skipping host configuration');
 	}
 
 /***/ },
