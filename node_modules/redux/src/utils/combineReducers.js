004c54214dbefd46204985d2510496b315777fc3
Stuart Colville <scolville@mozilla.com>
2015-10-20 09:55:55 +0100

    Commit redux node_modules for static deps

--- node_modules/redux/src/utils/combineReducers.js
+++ node_modules/redux/src/utils/combineReducers.js
@@ -5,7 +5,7 @@ import pick from '../utils/pick';
 
 /* eslint-disable no-console */
 
-function getErrorMessage(key, action) {
+function getUndefinedStateErrorMessage(key, action) {
   var actionType = action && action.type;
   var actionName = actionType && `"${actionType.toString()}"` || 'an action';
 
@@ -15,38 +15,68 @@ function getErrorMessage(key, action) {
   );
 }
 
-function verifyStateShape(initialState, currentState) {
-  var reducerKeys = Object.keys(currentState);
+function getUnexpectedStateKeyWarningMessage(inputState, outputState, action) {
+  var reducerKeys = Object.keys(outputState);
+  var argumentName = action && action.type === ActionTypes.INIT ?
+    'initialState argument passed to createStore' :
+    'previous state received by the reducer';
 
   if (reducerKeys.length === 0) {
-    console.error(
+    return (
       'Store does not have a valid reducer. Make sure the argument passed ' +
       'to combineReducers is an object whose values are reducers.'
     );
-    return;
   }
 
-  if (!isPlainObject(initialState)) {
-    console.error(
-      'initialState has unexpected type of "' +
-      ({}).toString.call(initialState).match(/\s([a-z|A-Z]+)/)[1] +
-      '". Expected initialState to be an object with the following ' +
+  if (!isPlainObject(inputState)) {
+    return (
+      `The ${argumentName} has unexpected type of "` +
+      ({}).toString.call(inputState).match(/\s([a-z|A-Z]+)/)[1] +
+      `". Expected argument to be an object with the following ` +
       `keys: "${reducerKeys.join('", "')}"`
     );
-    return;
   }
 
-  var unexpectedKeys = Object.keys(initialState).filter(
+  var unexpectedKeys = Object.keys(inputState).filter(
     key => reducerKeys.indexOf(key) < 0
   );
 
   if (unexpectedKeys.length > 0) {
-    console.error(
+    return (
       `Unexpected ${unexpectedKeys.length > 1 ? 'keys' : 'key'} ` +
-      `"${unexpectedKeys.join('", "')}" in initialState will be ignored. ` +
-      `Expected to find one of the known reducer keys instead: "${reducerKeys.join('", "')}"`
+      `"${unexpectedKeys.join('", "')}" found in ${argumentName}. ` +
+      `Expected to find one of the known reducer keys instead: ` +
+      `"${reducerKeys.join('", "')}". Unexpected keys will be ignored.`
     );
   }
+}
+
+function assertReducerSanity(reducers) {
+  Object.keys(reducers).forEach(key => {
+    var reducer = reducers[key];
+    var initialState = reducer(undefined, { type: ActionTypes.INIT });
+
+    if (typeof initialState === 'undefined') {
+      throw new Error(
+        `Reducer "${key}" returned undefined during initialization. ` +
+        `If the state passed to the reducer is undefined, you must ` +
+        `explicitly return the initial state. The initial state may ` +
+        `not be undefined.`
+      );
+    }
+
+    var type = '@@redux/PROBE_UNKNOWN_ACTION_' + Math.random().toString(36).substring(7).split('').join('.');
+    if (typeof reducer(undefined, { type }) === 'undefined') {
+      throw new Error(
+        `Reducer "${key}" returned undefined when probed with a random type. ` +
+        `Don't try to handle ${ActionTypes.INIT} or other actions in "redux/*" ` +
+        `namespace. They are considered private. Instead, you must return the ` +
+        `current state for any unknown actions, unless it is undefined, ` +
+        `in which case you must return the initial state, regardless of the ` +
+        `action type. The initial state may not be undefined.`
+      );
+    }
+  });
 }
 
 /**
@@ -68,47 +98,34 @@ function verifyStateShape(initialState, currentState) {
 
 export default function combineReducers(reducers) {
   var finalReducers = pick(reducers, (val) => typeof val === 'function');
+  var sanityError;
 
-  Object.keys(finalReducers).forEach(key => {
-    var reducer = finalReducers[key];
-    if (typeof reducer(undefined, { type: ActionTypes.INIT }) === 'undefined') {
-      throw new Error(
-        `Reducer "${key}" returned undefined during initialization. ` +
-        `If the state passed to the reducer is undefined, you must ` +
-        `explicitly return the initial state. The initial state may ` +
-        `not be undefined.`
-      );
-    }
-
-    var type = Math.random().toString(36).substring(7).split('').join('.');
-    if (typeof reducer(undefined, { type }) === 'undefined') {
-      throw new Error(
-        `Reducer "${key}" returned undefined when probed with a random type. ` +
-        `Don't try to handle ${ActionTypes.INIT} or other actions in "redux/*" ` +
-        `namespace. They are considered private. Instead, you must return the ` +
-        `current state for any unknown actions, unless it is undefined, ` +
-        `in which case you must return the initial state, regardless of the ` +
-        `action type. The initial state may not be undefined.`
-      );
-    }
-  });
+  try {
+    assertReducerSanity(finalReducers);
+  } catch (e) {
+    sanityError = e;
+  }
 
   var defaultState = mapValues(finalReducers, () => undefined);
-  var stateShapeVerified;
 
   return function combination(state = defaultState, action) {
+    if (sanityError) {
+      throw sanityError;
+    }
+
     var finalState = mapValues(finalReducers, (reducer, key) => {
       var newState = reducer(state[key], action);
       if (typeof newState === 'undefined') {
-        throw new Error(getErrorMessage(key, action));
+        var errorMessage = getUndefinedStateErrorMessage(key, action);
+        throw new Error(errorMessage);
       }
       return newState;
     });
 
     if (process.env.NODE_ENV !== 'production') {
-      if (!stateShapeVerified) {
-        verifyStateShape(state, finalState);
-        stateShapeVerified = true;
+      var warningMessage = getUnexpectedStateKeyWarningMessage(state, finalState, action);
+      if (warningMessage) {
+        console.error(warningMessage);
       }
     }
 
