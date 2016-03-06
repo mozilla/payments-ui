b51c36e4c42a6ececcde8518661c3ef47bf28cd5
Payments <payments-devs-automation@mozilla.com>
2016-03-06 13:19:59 +0000

    publish docker build branch (auto)
    branch:       greenkeeper-redux-thunk-2.0.0
    SHA:          88ce2bf977d9ebebffba714492a8ec6a15d55ee9
    range SHA:
    build id:     114041667
    build number: 1407

--- public/dist/transaction.bundle.js
+++ public/dist/transaction.bundle.js
@@ -44935,18 +44935,22 @@
 
 	'use strict';
 	
+	exports.__esModule = true;
+	exports['default'] = thunkMiddleware;
 	function thunkMiddleware(_ref) {
 	  var dispatch = _ref.dispatch;
 	  var getState = _ref.getState;
 	
 	  return function (next) {
 	    return function (action) {
-	      return typeof action === 'function' ? action(dispatch, getState) : next(action);
+	      if (typeof action === 'function') {
+	        return action(dispatch, getState);
+	      }
+	
+	      return next(action);
 	    };
 	  };
 	}
-	
-	module.exports = thunkMiddleware;
 
 /***/ },
 /* 487 */
