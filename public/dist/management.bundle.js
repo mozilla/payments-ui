1df0d3fa2a102bf713d2555e376b7743e5e79166
Payments <payments-devs-automation@mozilla.com>
2016-03-07 10:20:12 +0000

    publish docker build branch (auto)
    branch:       greenkeeper-karma-mocha-reporter-1.3.0
    SHA:          72c6e6768432d2809f236f092d7ffe12d864f91f
    range SHA:
    build id:     114201876
    build number: 1411

--- public/dist/management.bundle.js
+++ public/dist/management.bundle.js
@@ -47354,22 +47354,18 @@ webpackJsonp([0,2],[
 
 	'use strict';
 	
-	exports.__esModule = true;
-	exports['default'] = thunkMiddleware;
 	function thunkMiddleware(_ref) {
 	  var dispatch = _ref.dispatch;
 	  var getState = _ref.getState;
 	
 	  return function (next) {
 	    return function (action) {
-	      if (typeof action === 'function') {
-	        return action(dispatch, getState);
-	      }
-	
-	      return next(action);
+	      return typeof action === 'function' ? action(dispatch, getState) : next(action);
 	    };
 	  };
 	}
+	
+	module.exports = thunkMiddleware;
 
 /***/ },
 /* 487 */
