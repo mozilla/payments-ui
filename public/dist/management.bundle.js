66d8b04bbad896a403875ec17ed242cb92f17c53
Payments <payments-devs-automation@mozilla.com>
2016-04-06 23:39:31 +0000

    publish docker build branch (auto)
    branch:       greenkeeper-eslint-plugin-react-4.3.0
    SHA:          81de9112114cba50cb27dacb9603d0cefe161345
    range SHA:
    build id:     121299966
    build number: 1488

--- public/dist/management.bundle.js
+++ public/dist/management.bundle.js
@@ -27982,7 +27982,7 @@ webpackJsonp([0,2],[
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
-	 * jQuery JavaScript Library v2.2.3
+	 * jQuery JavaScript Library v2.2.1
 	 * http://jquery.com/
 	 *
 	 * Includes Sizzle.js
@@ -27992,7 +27992,7 @@ webpackJsonp([0,2],[
 	 * Released under the MIT license
 	 * http://jquery.org/license
 	 *
-	 * Date: 2016-04-05T19:26Z
+	 * Date: 2016-02-22T19:11Z
 	 */
 	
 	(function( global, factory ) {
@@ -28048,7 +28048,7 @@ webpackJsonp([0,2],[
 	
 	
 	var
-		version = "2.2.3",
+		version = "2.2.1",
 	
 		// Define a local copy of jQuery
 		jQuery = function( selector, context ) {
@@ -28259,7 +28259,6 @@ webpackJsonp([0,2],[
 		},
 	
 		isPlainObject: function( obj ) {
-			var key;
 	
 			// Not plain objects:
 			// - Any object or value whose internal [[Class]] property is not "[object Object]"
@@ -28269,18 +28268,14 @@ webpackJsonp([0,2],[
 				return false;
 			}
 	
-			// Not own constructor property must be Object
 			if ( obj.constructor &&
-					!hasOwn.call( obj, "constructor" ) &&
-					!hasOwn.call( obj.constructor.prototype || {}, "isPrototypeOf" ) ) {
+					!hasOwn.call( obj.constructor.prototype, "isPrototypeOf" ) ) {
 				return false;
 			}
 	
-			// Own properties are enumerated firstly, so to speed up,
-			// if last one is own, then all properties are own
-			for ( key in obj ) {}
-	
-			return key === undefined || hasOwn.call( obj, key );
+			// If the function hasn't returned already, we're confident that
+			// |obj| is a plain object, created by {} or constructed with new Object
+			return true;
 		},
 	
 		isEmptyObject: function( obj ) {
@@ -35313,12 +35308,6 @@ webpackJsonp([0,2],[
 		}
 	} );
 	
-	// Support: IE <=11 only
-	// Accessing the selectedIndex property
-	// forces the browser to respect setting selected
-	// on the option
-	// The getter ensures a default option is selected
-	// when in an optgroup
 	if ( !support.optSelected ) {
 		jQuery.propHooks.selected = {
 			get: function( elem ) {
@@ -35327,16 +35316,6 @@ webpackJsonp([0,2],[
 					parent.parentNode.selectedIndex;
 				}
 				return null;
-			},
-			set: function( elem ) {
-				var parent = elem.parentNode;
-				if ( parent ) {
-					parent.selectedIndex;
-	
-					if ( parent.parentNode ) {
-						parent.parentNode.selectedIndex;
-					}
-				}
 			}
 		};
 	}
@@ -35531,8 +35510,7 @@ webpackJsonp([0,2],[
 	
 	
 	
-	var rreturn = /\r/g,
-		rspaces = /[\x20\t\r\n\f]+/g;
+	var rreturn = /\r/g;
 	
 	jQuery.fn.extend( {
 		val: function( value ) {
@@ -35608,15 +35586,9 @@ webpackJsonp([0,2],[
 			option: {
 				get: function( elem ) {
 	
-					var val = jQuery.find.attr( elem, "value" );
-					return val != null ?
-						val :
-	
-						// Support: IE10-11+
-						// option.text throws exceptions (#14686, #14858)
-						// Strip and collapse whitespace
-						// https://html.spec.whatwg.org/#strip-and-collapse-whitespace
-						jQuery.trim( jQuery.text( elem ) ).replace( rspaces, " " );
+					// Support: IE<11
+					// option.value not trimmed (#14858)
+					return jQuery.trim( elem.value );
 				}
 			},
 			select: {
@@ -35669,7 +35641,7 @@ webpackJsonp([0,2],[
 					while ( i-- ) {
 						option = options[ i ];
 						if ( option.selected =
-							jQuery.inArray( jQuery.valHooks.option.get( option ), values ) > -1
+								jQuery.inArray( jQuery.valHooks.option.get( option ), values ) > -1
 						) {
 							optionSet = true;
 						}
@@ -37364,6 +37336,18 @@ webpackJsonp([0,2],[
 	
 	
 	
+	// Support: Safari 8+
+	// In Safari 8 documents created via document.implementation.createHTMLDocument
+	// collapse sibling forms: the second one becomes a child of the first one.
+	// Because of that, this security measure has to be disabled in Safari 8.
+	// https://bugs.webkit.org/show_bug.cgi?id=137337
+	support.createHTMLDocument = ( function() {
+		var body = document.implementation.createHTMLDocument( "" ).body;
+		body.innerHTML = "<form></form><form></form>";
+		return body.childNodes.length === 2;
+	} )();
+	
+	
 	// Argument "data" should be string of html
 	// context (optional): If specified, the fragment will be created in this context,
 	// defaults to document
@@ -37376,7 +37360,12 @@ webpackJsonp([0,2],[
 			keepScripts = context;
 			context = false;
 		}
-		context = context || document;
+	
+		// Stop scripts or inline event handlers from being executed immediately
+		// by using document.implementation
+		context = context || ( support.createHTMLDocument ?
+			document.implementation.createHTMLDocument( "" ) :
+			document );
 	
 		var parsed = rsingleTag.exec( data ),
 			scripts = !keepScripts && [];
@@ -37458,7 +37447,7 @@ webpackJsonp([0,2],[
 			// If it fails, this function gets "jqXHR", "status", "error"
 			} ).always( callback && function( jqXHR, status ) {
 				self.each( function() {
-					callback.apply( this, response || [ jqXHR.responseText, status, jqXHR ] );
+					callback.apply( self, response || [ jqXHR.responseText, status, jqXHR ] );
 				} );
 			} );
 		}
